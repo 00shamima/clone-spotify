9e15472d44e17832c587a7a5ad57d5a738c084d0
Shamima <shamima2802@gmail.com>
2025-09-29 15:04:51 +0530

    updates

---
+++ src/assets.js
@@ -0,0 +1,30 @@
+// src/assets.js
+
+// Use string paths starting with /icons/ to point to the files in the public/icons folder
+export const assets = {
+    home_icon: '/icons/home.png',
+    search_icon: '/icons/search.png',
+    stack_icon: '/icons/stack.png',  
+    arrow_icon: '/icons/arrow.png', 
+    plus_icon: '/icons/plus.png',
+    arrow_left: '/icons/left_arrow.png', 
+    arrow_right: '/icons/right_arrow.png',
+    bell_icon: '/icons/bell.png',
+    like_icon: '/icons/like.png',
+    loop_icon: '/icons/loop.png',
+    mic_icon: '/icons/mic.png',
+    next_icon: '/icons/next.png',
+    play_icon: '/icons/play.png',
+    pause_icon: '/icons/pause.png',
+    plays_icon: '/icons/plays.png',
+    prev_icon: '/icons/prev.png',
+    shuffle_icon: '/icons/shuffle.png',
+    speaker_icon: '/icons/speaker.png',
+    zoom_icon: '/icons/zoom.png',
+    mini_player_icon: '/icons/mini-player.png',
+    queue_icon: '/icons/queue.png',
+    volume_icon: '/icons/volume.png',
+    spotify_logo: '/icons/spotify_logo.png',
+    clock_icon: '/icons/clock_icon.png',
+    // ... all other icons must be listed here
+};
